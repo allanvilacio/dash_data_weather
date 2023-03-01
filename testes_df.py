bf8d0dfbae4e705fd14a535e493f2dc064c297c3
Allan Moreira <allan.vilacio@gmail.com>
2023-03-01 17:39:56 -0300

    Continuacao da construcao

--- testes_df.py
+++ testes_df.py
@@ -6,8 +6,4 @@ d_cidades = get_d_cidades()
 
 
 
-print(d_cidades.columns)
-
-[{'label': label, 'value': value} for label, value in d_cidades[['nome_mun','codigo_ibge']].sort_values(by='nome_mun').values]
-    
-print(df_weather['days_datetime'].max().strftime('%d de %B de %Y'))
+print(df_weather['days_temp'].mean())
