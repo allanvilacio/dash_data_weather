dd397dc89ce7bdec7df2d8c78067b8214fe8f6ea
Allan Moreira <allan.vilacio@gmail.com>
2023-03-07 17:45:58 -0300

    commit

--- app.py
+++ app.py
@@ -1,48 +1,63 @@
-from dash import Dash, html, dcc, Input, Output, State, callback
+from dash import Dash, html, dcc
 import dash
 import dash_bootstrap_components as dbc
+from utility.data_frames import get_d_cidades
+
+# Data frames
+d_cidades = get_d_cidades()
 
 
 app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.CYBORG])
 
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "13rem",
+    "padding": "1rem 1rem",
+    "background-color": "#424242",
+}
+
+CONTENT_STYLE = {
+    "margin-left": "13rem",
+    "margin-right": "1rem",
+    "padding": "1rem 1rem",
+}
+
 app.layout = dbc.Container(
     [
-        dbc.Button(
-            "Open scrollable offcanvas",
-            id="open-offcanvas-scrollable",
-            n_clicks=0,
-        ),
-        dbc.Offcanvas(
+        html.Div(
             [
-                html.P("The contents on the main page are now scrollable."),
+                html.H2("Sidebar"),
+                html.Hr(),
                 dbc.Nav(
                     [
-                        dbc.NavLink(page['name'], href= page["relative_path"], active="exact") for page in dash.page_registry.values()
-
+                        dbc.NavLink(page['name'], href= page["relative_path"], active="exact")
+			            for page in dash.page_registry.values()
                     ],
                     vertical=True,
                     pills=True,
-                )
+                ),
+		        html.Hr(),
+                html.H5('Filtro'),
+
+                dcc.Dropdown(id='location-dropdown', 
+                    options=[{'label': label, 'value': value} 
+			        for label, value in d_cidades[['nome_mun','codigo_ibge']].sort_values(by='nome_mun').values],
+                    value='2704302')
             ],
-            id="offcanvas-scrollable",
-            scrollable=True,
-            title="Scrollable Offcanvas",
-            is_open=False,
+            style=SIDEBAR_STYLE,
         ),
-        html.H1('Multi-page app with Dash Pages', style={'text-align':'center'}),
-        dash.page_container,              
+        html.Div(
+            [
+                dash.page_container,  
+
+            ], style=CONTENT_STYLE
+        )
+            
     ], fluid= True
 )
-
-@callback(
-    Output("offcanvas-scrollable", "is_open"),
-    Input("open-offcanvas-scrollable", "n_clicks"),
-    State("offcanvas-scrollable", "is_open"),
-)
-def toggle_offcanvas_scrollable(n1, is_open):
-    if n1:
-        return not is_open
-    return is_open
 
 if __name__ == '__main__':
 	app.run_server(debug=True)
--- pages/home.py
+++ pages/home.py
@@ -1,5 +1,5 @@
 import dash
-from dash import Dash, dcc ,html, Input, Output, State, callback
+from dash import Dash, dcc ,html, Input, Output, callback
 import dash_bootstrap_components as dbc
 import plotly.express as px
 
@@ -7,19 +7,15 @@ from utility.data_frames import get_df_weather, get_d_cidades
 
 # Data frames
 df_weather = get_df_weather()
-d_cidades = get_d_cidades()
 
 dash.register_page(__name__, path='/')
+
+STYLE_CARDS = {"margin-top": "10px", 'text-align':'center'}
 
 layout = dbc.Container(
     [
         dbc.Row(
             [
-                dbc.Col(
-                    dcc.Dropdown(id='location-dropdown', 
-                                 options=[{'label': label, 'value': value} for label, value in d_cidades[['nome_mun','codigo_ibge']].sort_values(by='nome_mun').values],
-                                 value='2704302'), md=3
-                ),
                 dbc.Col(html.P(id='teste-ibge'), md=3)
             ]
         ),
@@ -31,41 +27,36 @@ layout = dbc.Container(
                                 html.Span('tempmax', className="card-text"),
                                 html.H3( style={"color": "#adfc92"}, id='card-tempmax')
                                 ]), color='light', outline=True, 
-                                style={"margin-top": "10px",
-                                    "box-shadow": "0 4px 4px 0 rgba(0, 0, 0, 0.15), 0 4px 20px 0 rgba(0, 0, 0, 0.19)",
-                                    "color": "#FFFFFF",'text-align':'center'})),
+                                style=STYLE_CARDS)
+                ),
                 dbc.Col(dbc.Card(
                             dbc.CardBody([
                                 html.Span('temp', className="card-text"),
                                 html.H3(style={"color": "#adfc92"}, id='card-temp')
                                 ]), color="light", outline=True, 
-                                style={"margin-top": "10px",
-                                    "box-shadow": "0 4px 4px 0 rgba(0, 0, 0, 0.15), 0 4px 20px 0 rgba(0, 0, 0, 0.19)",
-                                    "color": "#FFFFFF",'text-align':'center'})),
+                                style=STYLE_CARDS)
+                ),
                 dbc.Col(dbc.Card(
                             dbc.CardBody([
                                 html.Span('tempmin', className="card-text"),
                                 html.H3(style={"color": "#adfc92"}, id='card-tempmin')
                                 ]), color="light", outline=True,
-                                style={"margin-top": "10px",
-                                    "box-shadow": "0 4px 4px 0 rgba(0, 0, 0, 0.15), 0 4px 20px 0 rgba(0, 0, 0, 0.19)",
-                                    "color": "#FFFFFF",'text-align':'center'})),
+                                style=STYLE_CARDS)
+                ),
                 dbc.Col(dbc.Card( 
                             dbc.CardBody([
                                 html.Span('windspeed', className="card-text"),
                                 html.H3(style={"color": "#adfc92"}, id='card-windspeed')
                                 ]), color="light", outline=True,
-                                style={"margin-top": "10px",
-                                    "box-shadow": "0 4px 4px 0 rgba(0, 0, 0, 0.15), 0 4px 20px 0 rgba(0, 0, 0, 0.19)",
-                                    "color": "#FFFFFF",'text-align':'center'})),
+                                style=STYLE_CARDS)
+                ),
                 dbc.Col(dbc.Card( 
                             dbc.CardBody([
                                 html.Span('humidity', className="card-text"),
                                 html.H3(style={"color": "#adfc92"}, id='card-humidity')
                                 ]), color="light", outline=True,
-                                style={"margin-top": "10px",
-                                    "box-shadow": "0 4px 4px 0 rgba(0, 0, 0, 0.15), 0 4px 20px 0 rgba(0, 0, 0, 0.19)",
-                                    "color": "#FFFFFF", 'text-align':'center'}))
+                                style=STYLE_CARDS)
+                )
             ]
         ),
         dbc.Row(
@@ -92,20 +83,12 @@ layout = dbc.Container(
                 html.Footer(f"Dados atualizados até: {df_weather['days_datetime'].max().strftime('%d de %B de %Y')}.")
             ], style={'margin': '10px', 'text-align':'right'}
         )
-    ], fluid= True
+    ], fluid=True
 )
-
-
 
 
 @callback(
      [
-        Output('teste-ibge', 'children'),
-        Output('card-tempmax', 'children'),
-        Output('card-temp', 'children'),
-        Output('card-tempmin', 'children'),
-        Output('card-windspeed', 'children'),
-        Output('card-humidity', 'children'),
         Output('example-graph1', 'figure'),
         Output('example-graph2', 'figure'),
         Output('example-graph3', 'figure')
@@ -115,12 +98,6 @@ layout = dbc.Container(
 def filtered_dfs(value):
 
     df_weather_filtered = df_weather[df_weather['codigo_ibge']==value]
-    
-    # ======= Cards =======================
-    
-    lista_cards = []
-    for column in ['days_tempmax', 'days_temp', 'days_tempmin', 'days_windspeed', 'days_humidity']:
-        lista_cards.append(df_weather_filtered.loc[df_weather_filtered.index.max()][column])
     
     # =========== Figure =======================
     fig1 = px.histogram(df_weather_filtered,
@@ -159,13 +136,36 @@ def filtered_dfs(value):
                 x='days_datetime',
                 y='days_temp',
                 color='ano',
-                title='Fig 1',
-                height=800)
+                title='Fig 1')
     
     fig3.update_traces({'hovertemplate':'%{y:.3}</b>', 'line_shape':'spline'} )
     fig3.update_layout({'hovermode':'x unified', "template":"plotly_dark"},
                       margin=dict(l=10, r=10, b=10, t=50))
 
     
+    return (fig1, fig2, fig3)
+
+
+@callback(
+     [
+        Output('teste-ibge', 'children'),
+        Output('card-tempmax', 'children'),
+        Output('card-temp', 'children'),
+        Output('card-tempmin', 'children'),
+        Output('card-windspeed', 'children'),
+        Output('card-humidity', 'children')
+    ],
+    [Input('location-dropdown', 'value')]
+)
+def filtered_cards(value):
+
+    df_weather_filtered = df_weather[df_weather['codigo_ibge']==value]
+    
+    # ======= Cards =======================
+    
+    lista_cards = []
+    for column in ['days_tempmax', 'days_temp', 'days_tempmin', 'days_windspeed', 'days_humidity']:
+        lista_cards.append(df_weather_filtered.loc[df_weather_filtered.index.max()][column])
+    
     return (value, lista_cards[0], lista_cards[1], lista_cards[2], 
-            lista_cards[3], lista_cards[4], fig1, fig2, fig3)
+            lista_cards[3], lista_cards[4])
